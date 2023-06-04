ce32b74153a62438d9455dc722a99f3e0bd393e9
Broxxlee <134107615+Broxxlee@users.noreply.github.com>
2023-06-04 17:00:06 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,110 +1,130 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const indicatorSelect = document.getElementById('indicatorSelect');
-  const indicatorInfo = document.getElementById('indicatorInfo');
-  const indicatorChart = document.getElementById('indicatorChart').getContext('2d');
-  let chart;
+  var currencySelector = document.getElementById('currency');
+  var yearSelector = document.getElementById('year');
+  var dateInput = document.getElementById('date');
+  var searchButton = document.getElementById('search-btn');
 
-  // Obtener los tipos de indicadores disponibles
+  searchButton.addEventListener('click', fetchData);
+
+  function fetchData() {
+    var currency = currencySelector.value;
+    var year = yearSelector.value;
+
+    // Obtener el último día del mes seleccionado
+    var lastDay = new Date(year, dateInput.value.split('-')[1], 0).getDate();
+
+    dateInput.setAttribute('max', year + '-12-' + lastDay);
+
+    var url = 'https://mindicador.cl/api/' + currency + '/' + year;
+
+    if (dateInput.value) {
+      url += '/' + formatDate(dateInput.value);
+    }
+
+    axios.get(url)
+      .then(function(response) {
+        var data = response.data;
+        var labels = [];
+        var values = [];
+
+        if (dateInput.value) {
+          labels.push(formatDate(dateInput.value));
+          values.push(data.serie[0].valor);
+        } else {
+          for (var i = 0; i < data.serie.length; i++) {
+            var item = data.serie[i];
+            labels.push(formatDate(item.fecha));
+            values.push(item.valor);
+          }
+        }
+
+        renderChart(labels, values);
+        renderTable(data.serie);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  function renderChart(labels, values) {
+    var ctx = document.getElementById('chart').getContext('2d');
+
+    var chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Valor',
+          data: values,
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1
+        }]
+      },
+      options: {
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Fecha'
+            }
+          },
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Valor'
+            }
+          }
+        }
+      }
+    });
+  }
+
+  function renderTable(data) {
+    var tableBody = document.getElementById('table-body');
+    tableBody.innerHTML = '';
+
+    for (var i = 0; i < data.length; i++) {
+      var item = data[i];
+      var row = document.createElement('tr');
+      var currencyCell = document.createElement('td');
+      var valueCell = document.createElement('td');
+      var dateCell = document.createElement('td');
+
+      currencyCell.textContent = currencySelector.options[currencySelector.selectedIndex].text;
+      valueCell.textContent = item.valor;
+      dateCell.textContent = formatDate(item.fecha, false);
+
+      row.appendChild(currencyCell);
+      row.appendChild(valueCell);
+      row.appendChild(dateCell);
+
+      tableBody.appendChild(row);
+    }
+  }
+
+  function formatDate(date, includeWeekday) {
+    var parts = date.split('-');
+    var formattedDate = parts[2] + '-' + parts[1] + '-' + parts[0];
+
+    return formattedDate;
+  }
+
+  // Obtener las monedas disponibles de la API
   axios.get('https://mindicador.cl/api')
     .then(function(response) {
-      const indicators = response.data;
-      for (const key in indicators) {
-        const option = document.createElement('option');
-        option.value = key;
-        option.textContent = indicators[key].nombre;
-        indicatorSelect.appendChild(option);
+      var currencies = response.data;
+
+      for (var currency in currencies) {
+        var option = document.createElement('option');
+        option.value = currency;
+        option.textContent = currencies[currency].nombre;
+        currencySelector.appendChild(option);
       }
     })
     .catch(function(error) {
       console.log(error);
     });
-
-  // Obtener la información del indicador seleccionado
-  indicatorSelect.addEventListener('change', function() {
-    const selectedIndicator = indicatorSelect.value;
-    axios.get(`https://mindicador.cl/api/${selectedIndicator}`)
-      .then(function(response) {
-        const indicatorData = response.data;
-        const latestData = indicatorData.serie[0];
-
-        const html = `
-          <h3>${indicatorData.nombre}</h3>
-          <p>Valor: ${latestData.valor}</p>
-          <p>Fecha: ${latestData.fecha}</p>
-          <p>Unidad de medida: ${indicatorData.unidad_medida}</p>
-        `;
-        indicatorInfo.innerHTML = html;
-
-        const dates = [];
-        const values = [];
-        for (const data of indicatorData.serie) {
-          dates.push(data.fecha);
-          values.push(data.valor);
-        }
-
-        if (chart) {
-          chart.destroy();
-        }
-
-        chart = new Chart(indicatorChart, {
-          type: 'line',
-          data: {
-            labels: dates,
-            datasets: [{
-              label: indicatorData.nombre,
-              data: values,
-              backgroundColor: 'rgba(0, 123, 255, 0.3)',
-              borderColor: 'rgba(0, 123, 255, 1)',
-              borderWidth: 1
-            }]
-          },
-          options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            scales: {
-              x: {
-                display: true,
-                grid: {
-                  color: 'rgba(0, 0, 0, 0.1)', // Color de la línea de las coordenadas x
-                  borderColor: 'rgba(0, 0, 0, 0.1)' // Color del borde de las coordenadas x
-                },
-                ticks: {
-                  autoSkip: true,
-                  maxTicksLimit: 10,
-                  color: 'rgba(0, 0, 0, 0.5)', // Color de las coordenadas x
-                  font: {
-                    size: 12
-                  }
-                }
-              },
-              y: {
-                beginAtZero: true,
-                grid: {
-                  color: 'rgba(0, 0, 0, 0.1)' // Color de las coordenadas y
-                },
-                ticks: {
-                  color: 'rgba(0, 0, 0, 0.5)', // Color de las coordenadas y
-                  font: {
-                    size: 12
-                  }
-                }
-              }
-            },
-            plugins: {
-              legend: {
-                display: true,
-                labels: {
-                  font: {
-                    size: 14
-                  }
-                }
-              }
-            }
-          }
-        });
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  });
 });
